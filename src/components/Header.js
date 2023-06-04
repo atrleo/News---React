20844f19a3d64903a62e019f61403fee74e2b65b
abanishtr <abanisht558@gmail.com>
2023-06-04 16:46:37 +0545

    Added previous and next button

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,18 @@ export class Header extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="container-fluid"> 
-                        <a className="navbar-brand" href="#">DailyNews</a>
+                <nav className="navbar navbar-expand-lg bg-dark">
+                    <div className="container "> 
+                        <a className="navbar-brand text-white" href="#">DailyNews</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div className="navbar-nav">
-                                <a className="nav-link " aria-current="page" href="#">Home</a>
-                                <a className="nav-link" href="#">Features</a>
-                                <a className="nav-link" href="#">Pricing</a>
-                                
+                            <div className="navbar-nav " >
+                                <a className="nav-link text-white" aria-current="page" href="#">Home</a>
+                                <a className="nav-link text-white" href="#">Features</a>
+                                <a className="nav-link text-white" href="#">Pricing</a>
+
                             </div>
                         </div>
                     </div>
