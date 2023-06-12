4db9f0bf455cfc542adaabd08743db7fb6fcec6e
abanishtr <abanisht558@gmail.com>
2023-06-12 15:12:48 +0545

    added Custom envirnment variable and hide api

--- src/App.js
+++ src/App.js
@@ -19,27 +19,28 @@ import {
 
 export default class App extends Component {
   pageCount = 10;
+  apiKey= process.env.REACT_APP_NEWS_API;
   render() {
     return (
       <div>
         <Router>
           <Header />
           <Routes>
-            <Route exact path="/" element={<News key = "home" pageSize={this.pageCount} country="us" category= "general"/>}>   
+            <Route exact path="/" element={<News apiKey={this.apiKey} key = "home" pageSize={this.pageCount} country="us" category= "general"/>}>   
             </Route>
           
-            <Route exact path="/business" element={<News key = "business" pageSize={this.pageCount} country="us" category= "business"/>}>   
+            <Route exact path="/business" element={<News apiKey={this.apiKey} key = "business" pageSize={this.pageCount} country="us" category= "business"/>}>   
             </Route>
-            <Route exact path="/entertainment" element={<News key = "entertainment" pageSize={this.pageCount} country="us" category= "entertainment"/>}>   
+            <Route exact path="/entertainment" element={<News apiKey={this.apiKey} key = "entertainment" pageSize={this.pageCount} country="us" category= "entertainment"/>}>   
             </Route>
             
-            <Route exact path="/health" element={<News key = "health" pageSize={this.pageCount} country="us" category= "health"/>}>   
+            <Route exact path="/health" element={<News apiKey={this.apiKey} key = "health" pageSize={this.pageCount} country="us" category= "health"/>}>   
             </Route>
-            <Route exact path="/science" element={<News key = "science" pageSize={this.pageCount} country="us" category= "science"/>}>   
+            <Route exact path="/science" element={<News  apiKey={this.apiKey}key = "science" pageSize={this.pageCount} country="us" category= "science"/>}>   
             </Route>
-            <Route exact path="/sports" element={<News key = "sports" pageSize={this.pageCount} country="us" category= "sports"/>}>   
+            <Route exact path="/sports" element={<News  apiKey={this.apiKey}key = "sports" pageSize={this.pageCount} country="us" category= "sports"/>}>   
             </Route>
-            <Route exact path="/technology" element={<News key = "technology" pageSize={this.pageCount} country="us" category= "technology"/>}>   
+            <Route exact path="/technology" element={<News apiKey={this.apiKey} key = "technology" pageSize={this.pageCount} country="us" category= "technology"/>}>   
             </Route>
 
 
